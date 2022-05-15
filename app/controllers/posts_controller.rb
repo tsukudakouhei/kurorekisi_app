79906c8ab06f2973c3f9aaaceac5480108cf231c
tsukudakouhei <tsuku.pique.job@gmail.com>
2022-05-16 00:57:39 +0900

    User Create login Post finish

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,61 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user
+  before_action :ensure_correct_user,{only: [:edit, :update, :destroy]}
+  before_action :ensure_correct_user,{only: [:edit,:update]}
+
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @current_user.id != @post.user_id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+  
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    @user = @post.user
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id
+    )
+    if @post.save
+      flash[:notice] = "作成しました！"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "編集しました！"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "削除しました！"
+    redirect_to("/posts/index")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  
+  
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+
+  post 'users/:id/update' => "users#update"
+  get 'users/:id/edit' => "users#edit"
+  post 'users/create' => "users#create"
+  get 'signup' => "users#new"
+  get 'users/index' => "users#index"
+  get 'users/:id' => "users#show"
+  
+  
+
+  get 'posts/index' => "posts#index"
+  #showの前に書かないとエラーでる
+  get 'posts/new' => "posts#new"
+  post 'posts/create' => "posts#create"
+  get 'posts/:id' => "posts#show"
+  get 'posts/:id/edit' => "posts#edit"
+  post 'posts/:id/update' => "posts#update"
+  post 'posts/:id/destroy' => "posts#destroy"
+  
+
+  get '/' => "home#top"
+  
 end
